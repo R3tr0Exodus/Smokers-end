ac0e55669ee57ba29a159311183f8db91b675a0c
Magnus H <mfh1106@gmail.com>
2024-01-05 09:55:50 +0100

    Movement + size + init interaction

---
+++ My project/Assets/Scripts/InteractionSystem/Inventory.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Inventory : MonoBehaviour
+{
+    public bool HasKey = false;
+
+    private void Update()
+    {
+        if (Keyboard.current.qKey.wasPressedThisFrame)HasKey = !HasKey;
+        
+    }
+}
